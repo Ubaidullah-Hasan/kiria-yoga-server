a22199242cf36763baa23f0de2298c14ecc487ba
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-06-13 00:01:10 +0600

    dhsj

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const port = process.env.PORT || 4000;
 
 
 // use middlewire
-const corsConfig = {
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
+
 app.use(cors())
 app.use(express.json());
 
@@ -49,7 +45,8 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
         // create verify Admin middlewire
         const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
@@ -64,8 +61,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
         // ACCESS KEY JWT START
